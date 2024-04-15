3d526c253eba79da08b3f49591c7f97b5db7ce33
ankit <ankitkumar748846@gmail.com>
2024-04-16 05:29:36 +0530

    feat: Added denied page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import NotFound from "./Pages/NotFound";
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
 import Contact from "./Pages/Contact";
+import Denied from "./Pages/Denied";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/denied" element={<Denied />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </>
